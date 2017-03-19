e87cc2c3c493895e5ade5db7403a098d34f000cf
matt-winzer <matt.winzer@gmail.com>
2017-03-19 15:12:28 -0600

    modified route for get one sequence

--- routes/sequence.js
+++ routes/sequence.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var knex = require('../db/knex');
 const objection = require('objection');
 
-// router.get('/', (req, res, next) => {
-//   return knex('sequence')
-//       .then(data => {
-//         var result = {
-//           sequences: data
-//         };
-//         res.json(result);
-//       });
-// });
-//
+router.get('/', (req, res, next) => {
+  return knex('sequence')
+      .then(data => {
+        var result = {
+          sequences: data
+        };
+        res.json(result);
+      });
+});
+
 router.get('/:id', (req, res, next) => {
   let id = req.params.id;
   return knex('sequence').where('id', id).first()
@@ -20,8 +20,7 @@ router.get('/:id', (req, res, next) => {
         var result = {
           sequence: {
             id: data.id,
-            // yogiID: data.yogiID,
-            yogi: data.yogiID
+            yogiID: data.yogiID,
           }
         };
         res.json(result);
@@ -44,21 +43,21 @@ router.get('/:id', (req, res, next) => {
 //       });
 // });
 
-router.get('/', (req, res, next) => {
-  return knex('sequence')
-      .then(data => {
-        return Promise.all(data.map(sequence => {
-          return knex('yogi').where('id', sequence.yogiID).first()
-            .then(yogi => {
-              sequence.yogi = yogi.id;
-              delete sequence.yogiID;
-              return sequence;
-            });
-        }))
-      })
-      .then(sequences => {
-        res.json({sequences: sequences});
-      });
-});
+// router.get('/', (req, res, next) => {
+//   return knex('sequence')
+//       .then(data => {
+//         return Promise.all(data.map(sequence => {
+//           return knex('yogi').where('id', sequence.yogiID).first()
+//             .then(yogi => {
+//               sequence.yogi = yogi.id;
+//               delete sequence.yogiID;
+//               return sequence;
+//             });
+//         }))
+//       })
+//       .then(sequences => {
+//         res.json({sequences: sequences});
+//       });
+// });
 
 module.exports = router;
