ab5f3e39ceb6018e2800cb1a84065e003717d99b
matt-winzer <matt.winzer@gmail.com>
2017-03-23 12:54:23 -0600

    adjusted sequence migration added name, added sequence seeds

--- seeds/02_sequence.js
+++ seeds/02_sequence.js
@@ -1,13 +1,27 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('DELETE FROM "sequence"; ALTER SEQUENCE sequence_id_seq RESTART WITH 3;')
+  return knex.raw('DELETE FROM "sequence"; ALTER SEQUENCE sequence_id_seq RESTART WITH 6;')
     .then(function () {
       var sequences = [{
         id: 1,
-        yogiID: 1
+        yogiID: 1,
+        name: 'Relax Before Bed'
       }, {
         id: 2,
-        yogiID: 2
+        yogiID: 2,
+        name: 'Morning Warmup'
+      }, {
+        id: 3,
+        yogiID: 2,
+        name: 'Midday Stress Relief'
+      }, {
+        id: 4,
+        yogiID: 2,
+        name: 'Open the Hips'
+      }, {
+        id: 5,
+        yogiID: 1,
+        name: 'Morning Warmup'
       }];
       return knex('sequence').insert(sequences);
     });
